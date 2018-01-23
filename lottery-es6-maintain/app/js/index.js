136ca5ae165ea894fed0a6fc28ba308e677d9016
fangyukui <m13560050110@163.com>
2018-01-23 17:53:37 +0800

    bugfix

--- lottery-es6-maintain/app/js/index.js
+++ lottery-es6-maintain/app/js/index.js
@@ -1,9 +1,10 @@
 // import  './class/es6/01-td'
 // import './class/es6/02-td'
 import  'babel-polyfill'
-import './class/es6/11-td'
-import 'babel-runtime'
-import 'babel-plugin-transform-runtime'
+//import './class/es6/11-td'
+import './lottery'
+
+
 
 
 
